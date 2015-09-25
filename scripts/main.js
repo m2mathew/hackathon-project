b996b92b2981f3b000d3f3481ff631671808b954
Mike Mathew <m2mathew@me.com>
2015-09-25 11:09:12 -0500

    Added template functionality and new collections

--- scripts/main.js
+++ scripts/main.js
@@ -2,23 +2,22 @@
 
 var Backbone = require('backbone');
 var $ = require('jquery');
+var _ = require('backbone/node_modules/underscore');
+
 var SpotModel = require('./models/SpotModel.js');
 var RestaurantModel = require('./models/RestaurantModel.js');
 var UserModel = require('./models/UserModel.js');
 
-console.log('Models loaded');
-
-// var SpotCollection = require('./collections/SpotCollection.js');
-// var RestaurantCollection = require('./collections/RestaurantCollection.js');
-// var UserCollection = require('./collections/UserCollection.js');
-
+var SpotCollection = require('./collections/SpotCollection.js');
+var RestaurantCollection = require('./collections/RestaurantCollection.js');
+var UserCollection = require('./collections/UserCollection.js');
 
 $(document).ready(function() {
 
-    console.log('document is ready');
     var $login = $('#login');
     var $restaurants = $('#restaurants');
     var $parkingSpots = $('#parking_spots');
+    var restaurantTemplate = _.template($('#restaurant-list').html());
 
     var Router = Backbone.Router.extend({
         routes: {
@@ -42,5 +41,30 @@ $(document).ready(function() {
     $('nav').show();
     $('section').hide();
     $restaurants.show();
+
+    var restaurants = new RestaurantCollection();
+
+    // get the name of restaurant from each collection to put in the HTML element
+    restaurants.fetch();
+    restaurants.on('add', function(x) {
+        var restaurantName = x.get('name');
+        console.log(this);
+        $('#restaurant-list').append(restaurantName);
+        console.log(restaurantName);
+    });
+
+
+
+
+    $restaurants.on('click', function() {
+
+
+        restaurantTemplate(model.toJSON());
+        console.log(restaurantTemplate(model.toJSON()));
+
+    // restaurantTemplate()
+
+
+    });
 
 });
